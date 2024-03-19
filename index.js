521db24c0800aa54a771d5d24237b4ffa970e7ec
İsmail Bulut Gönülalan <bulutgonulalan@hotmail.com>
2024-03-19 20:15:28 +0300

    jQuery added

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ function game() {
   changeImage("2", player2);
 
   if (player1 > player2) {
-    $("h2").text = "Player 1 Win!";
-    $(".dice .p1").text = "🏆 Player 1";
-    $(".dice .p2").text = "Player 2";
+    $("h2").text("Player 1 Win!");
+    $(".dice .p1").text("🏆 Player 1");
+    $(".dice .p2").text("Player 2");
   } else if (player2 > player1) {
-    $("h2").text = "Player 2 Win!";
-    $(".dice .p1").text = "Player 1";
-    $(".dice .p2").text = "🏆 Player 2";
+    $("h2").text("Player 2 Win!");
+    $(".dice .p1").text("Player 1");
+    $(".dice .p2").text("🏆 Player 2");
   } else {
-    $("h2").text = "Draw 🤝🏼";
-    $(".dice .p1").text = "Player 1";
-    $(".dice .p2").text = "Player 2";
+    $("h2").text("Draw 🤝🏼");
+    $(".dice .p1").text("Player 1");
+    $(".dice .p2").text("Player 2");
   }
 }
 
